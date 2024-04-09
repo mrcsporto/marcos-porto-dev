a4425b06eff56fec84899ebcb39ece643de2b849
mrcsporto <mrcsporto@gmail.com>
2024-04-08 21:42:39 -0300

    UPDATE PROFILE AND PORTFOLIO

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,77 @@ AOS.init({
   duration: 1000
 });
 
+const portfolioItems = [
+  {
+    title: "CompreUp",
+    url: "https://compreup.com.br/",
+    description: "Desenvolvimento de Front-End e Reestruturação de Back-End em Plataforma de E-Procurement.Utilizei Angular para criar uma interface responsiva e interativa para nossa plataforma de E-Procurement, integrando funcionalidades avançadas com sistemas de ERP. No back-end, estou liderando a reestruturação usando Ruby on Rails, aumentando a escalabilidade e facilitando futuras integrações, garantindo uma solução robusta e eficiente.",
+    languageIcon: "devicon-typescript-plain colored icons-languages",
+    languageName: "Typescrpit",
+    badgeText: "Projeto Real",
+    badgeClass: "bg-success"
+  },
+  {
+    title: "Sistema de Administração Pública",
+    url: "https://gerencial.speedgov.com.br/maracanau",
+    description: "Sistemas para Gestão Pública para prefeituras do estado do Ceará que incluem módulos específicos para gestão administrativas, fiscais e contábeis de prefeituras. Como, Gestão do ISSQN, Tributária, Patrimonial, Licitação, Dívida Ativa ew etc. O Sistema Integrado Municipal (SIM) abrange essas funcionalidades de forma integrada, permitindo a gestão de todos os processos de forma automatizada.",
+    languageIcon: "icon-ruby",
+    languageName: "Ruby",
+    badgeText: "Projeto Real",
+    badgeClass: "bg-success"
+  },
+  {
+    title: "Sistema Controle Financeiro",
+    url: "http://financeiro.speedgov.com.br/usuarios/sign_in/",
+    description: "Desenvolvi integralmente um sistema personalizado para controle financeiro interno da empresa Intersol Soluções, que gerencia receitas, despesas e transferências. Ele também inclui funcionalidades gestão de acessos, logs de alteração, relatórios e dashboards, proporcionando uma visão detalhada da saúde financeira da organização.",
+    languageIcon: "icon-ruby",
+    languageName: "Ruby",
+    badgeText: "Projeto Real",
+    badgeClass: "bg-success"
+  },
+  {
+    title: "BestContent AI",
+    url: "https://bestcontent.ai/",
+    description: "Desenvolvi a parte do front-end da BestContent AI, que é uma plataforma projetada para criar e gerenciar conteúdo de mídia social com o auxílio de inteligência artificial. Além disso, contribuí para a integração com banco de dados Firebase e autenticação de usuários, bem como a integração com a Graph API da Meta para conectar a plataforma diretamente com redes sociais.",
+    languageIcon: "icon-javascript-alt",
+    languageName: "Javascript",
+    badgeText: "Projeto Real",
+    badgeClass: "bg-success"
+  },
+  {
+    title: "Bank Account ATM Machine",
+    url: "https://github.com/mrcsporto/atm-case",
+    description: "Aplicativo desenvolvido para simular a gestão de uma conta bancária e suas respectivas transações financeiras. Este aplicativo permite aos usuários visualizar e gerenciar depósitos, saques, transferências e outras operações bancárias de forma intuitiva e segura, proporcionando uma experiência realística do gerenciamento de contas bancárias.",
+    languageIcon: "icon-ruby",
+    languageName: "Ruby",
+    badgeText: "Projeto Estudo",
+    badgeClass: "bg-primary"
+  },
+  {
+    title: "Programming Community Forum",
+    url: "https://github.com/mrcsporto/forum-on-rails",
+    description: "Comunidade brasileira sobre desenvolvimento de software, que simula um espaço para profissionais e entusiastas da área trocarem conhecimentos, experiências e discutirem as últimas tendências e tecnologias do setor.",
+    languageIcon: "icon-ruby",
+    languageName: "Ruby",
+    badgeText: "Projeto Estudo",
+    badgeClass: "bg-primary"
+  },
+  {
+    title: "Inventory Control System API",
+    url: "https://github.com/mrcsporto/factory-api-v1",
+    description: "API REST pública simulada para gerenciar produtos, centros de inventário e pedidos. O projeto é um objeto de estudo de caso para explorar como APIs funcionam na prática, permitindo operações de CRUD (Criar, Ler, Atualizar, Deletar) em produtos, controle de estoque em vários centros e gestão de pedidos.",
+    languageIcon: "icon-ruby",
+    languageName: "Ruby",
+    badgeText: "Projeto Estudo",
+    badgeClass: "bg-primary"
+  },
+  {
+    title: "Tic-Tac-Toe Game",
+    url: "https://mrcsporto.github.io/tic-tac-toe/",
+    description: "Este projeto envolve o desenvolvimento de um jogo de 'Tic-tac-toe' (Jogo da Velha) projetado para ser jogado por dois jogadores no mesmo computador. A implementação se concentra em oferecer uma interface de usuário simples e intuitiva, garantindo uma experiência de jogo fluida e agradável.",
+    languageIcon: "icon-javascript-alt",
+    languageName: "Javascript",
+    badgeText: "Projeto Estudo",
+    badgeClass: "bg-primary"
+  }
+];
